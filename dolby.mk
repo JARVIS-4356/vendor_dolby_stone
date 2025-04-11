a2364a4f8caa62bc7953d50496d4537cffd9d790
JARVIS <41569276+JARVIS-4356@users.noreply.github.com>
2025-04-11 17:23:41 +0000

    Revert "dolby: Make dolby QPR2 Compatible" - Fixed in vendor
    This reverts commit 81875aa4b6737a1eec3fab5b3de3f5e19112f223.

--- dolby.mk
+++ dolby.mk
@@ -8,7 +8,7 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default.xml \
     $(LOCAL_PATH)/configs/dolby/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
-    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_holi/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_effects.xml \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
